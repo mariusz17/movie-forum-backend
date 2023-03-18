1d4b71535db962c20d39adf7e327df9333021241
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2023-03-18 19:11:28 +0100

    fix error message text

--- src/middleware/i18n/errorMessages/pl.ts
+++ src/middleware/i18n/errorMessages/pl.ts
@@ -5,7 +5,7 @@ export const pl: ErrorMessages = {
   internal: 'Błąd wewnętrzny serwera',
   includeFields: 'Proszę uzupełnić wszystkie pola',
   userExists: 'Użytkownik o podanym mailu już istnieje',
-  wrongCredentials: 'Podano niepoprawny login lub hasło',
+  wrongCredentials: 'Podano niepoprawny email lub hasło',
   invalidData: 'Nieprawidłowe dane',
   unauthorized: 'Brak uprawnień do żądanego zasobu',
   sessionExpired: 'Sesja wygasła, zaloguj się ponownie',
